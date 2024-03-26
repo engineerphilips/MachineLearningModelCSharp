170377be63ee005a8822128e221671296036b148
Philip Baba <engineerphilips@outlook.com>
2024-03-26 00:06:04 +0000

    Add project files.

---
+++ MachineLearningModelCSharp/IrisData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ML.Data;
+
+namespace MachineLearningModelCSharp
+{
+    internal class IrisData
+    {
+        [LoadColumn(0)] public float SepalLength;
+        [LoadColumn(1)] public float SepalWidth;
+        [LoadColumn(2)] public float PetalLength;
+        [LoadColumn(3)] public float PetalWidth;
+    }
+}
---
+++ MachineLearningModelCSharp/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.ML.Data;
+using System;
+using System.IO;
+using MachineLearningModelCSharp;
+using Microsoft.ML;
+
+var _dataPath = Path.Combine(Environment.CurrentDirectory, "Data", "iris.data");
+var _modelPath = Path.Combine(Environment.CurrentDirectory, "Data", "IrisClusteringModel.zip");
+
+var mlContext = new MLContext(seed: 0);
+var dataView = mlContext.Data.LoadFromTextFile<IrisData>(_dataPath, hasHeader: false, separatorChar: ',');
+
+var featuresColumnName = "Features";
+var pipeline = mlContext.Transforms
+    .Concatenate(featuresColumnName,
+        "SepalLength",
+        "SepalWidth",
+        "PetalLength",
+        "PetalWidth")
+    .Append(mlContext.Clustering.Trainers.KMeans(featuresColumnName, numberOfClusters: 3));
+
+var model = pipeline.Fit(dataView);
+
+using (var fileStream = new FileStream(_modelPath, FileMode.Create, FileAccess.Write, FileShare.Write))
+{
+    mlContext.Model.Save(model, dataView.Schema, fileStream);
+}
+
+var predictor = mlContext.Model.CreatePredictionEngine<IrisData, ClusterPrediction>(model);
+
+var prediction = predictor.Predict(TestIrisData.Setosa);
+Console.WriteLine($"Cluster: {prediction.PredictedClusterId}");
+Console.WriteLine($"Distance: {string.Join(" ", prediction.Distances)}");
+Console.ReadLine();
